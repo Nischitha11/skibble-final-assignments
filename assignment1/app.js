275e13bde056412df1fa884d498a4bc84424bb75
Nischitha11 <nischu116@gmail.com>
2022-03-23 10:05:20 +0530

    my first commit

---
+++ assignment1/app.js
@@ -0,0 +1,5 @@
+const ele = document.getElementsByTagName('body');
+function changeBgColor(event) {
+    const bgColor = event.target.value;
+    ele[0].style.backgroundColor = bgColor;
+}
---
+++ assignment2/app.js
@@ -0,0 +1,22 @@
+const value = document.querySelector("#value");
+const btnDec = document.querySelector("#btn1");
+const btnReset = document.querySelector("#btn2");
+const btnInc = document.querySelector("#btn3");
+let count=0;
+btnDec.addEventListener("click", function(){
+    value.textContent = --count;
+    if(count<0){
+        value.style.color = "brown";
+    }
+})
+btnReset.addEventListener("click", function(){
+    count=0;
+    value.textContent=0;
+    value.style.color = "black";
+})
+btnInc.addEventListener("click", function(){
+    value.textContent = ++count;
+    if(count>0){
+        value.style.color = "blue";
+    }
+})
